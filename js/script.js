5c326da3ae50a8797b4953c14d7ab84151512d14
RetaxMaster <retaxmaster@hotmail.com>
2019-06-02 03:04:25 -0500

    Se transpilo el codigo ECMAScript a JavaScript

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 //Cuando el documento este cargado entonces que cargue el código JavaScript
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function(){
 
     //Abrir y cerrar el menú
-    document.querySelector("#burger-menu").addEventListener("click", function() {
-        const elements= document.querySelector("nav .elements");
+    document.getElementById("burger-menu").addEventListener("click", function() {
+        console.log(document.getElementsByTagName("nav")[0]);
+        
+        var elements= document.getElementsByTagName("nav")[0].children[1];
 
         if (elements.style.display == "flex") {
             elements.setAttribute("style", "display: none !important");
@@ -16,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
     //Mostrar el menú siempre que se cambie el tamaño de la pantalla
     window.addEventListener("resize", function(e) {
         if (window.innerWidth > 1000) {
-            document.querySelector("nav .elements").removeAttribute("style");
+            document.getElementsByTagName("nav")[0].children[1].removeAttribute("style");
         }
     })
 
--- js/slider.js
+++ js/slider.js
@@ -1,39 +1,58 @@
 //Cuando el documento este cargado entonces que cargue el código JavaScript
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", function() {
 
-    //Agrego el evento a todas las flechas hacia la izquierda de los sliders
-    document.querySelectorAll(".slider-container .left").forEach(leftArrow => {
-        leftArrow.addEventListener("click", function() {
-            const carrete = this.parentNode.children[1].children[0];
-            let marginLeft = parseInt(carrete.style.marginLeft);
+    //Obtengo todos los sliders
+    var slider = document.getElementsByClassName("slider-container");
 
-            if (isNaN(marginLeft)) {
-                marginLeft = 0;
-            }
-            
-            const nuevoMargen = marginLeft + 100;
-            if (nuevoMargen >= -200 && nuevoMargen <= 0) {
-                carrete.style.marginLeft = nuevoMargen + "%";
-            }
-        });
-    });
+    //Se recorre cada slider
+    for (let i = 0; i < slider.length; i++) {
 
-    //Agrego el evento a todas las flechas hacia la derecha de los sliders
-    document.querySelectorAll(".slider-container .right").forEach(rightArrow => {
-        rightArrow.addEventListener("click", function () {
-            const carrete = this.parentNode.children[1].children[0];
-            let marginLeft = parseInt(carrete.style.marginLeft);
+        //Obtengo las flechas apuntando a la izquierda de cada slider
+        var leftArrows = slider[i].getElementsByClassName("left");
+        
+        //Recorro cada una de esas flechas
+        for (var j = 0; j < leftArrows.length; j++) {
 
-            if (isNaN(marginLeft)) {
-                marginLeft = 0;
-            }
+            //A cada flecha le agrego un evento click
+            leftArrows[i].addEventListener("click", function() {
+                //Obtengo el carrete y obtengo su margen izquierdo y lo convierto a entero
+                var carrete = this.parentNode.children[1].children[0];
+                var marginLeft = parseInt(carrete.style.marginLeft);
+                
+                //Si no existe el margen entonces lo pongo en 0
+                if (isNaN(marginLeft)) {
+                    marginLeft = 0;
+                }
+                
+                //Le agrego 100 px de margen al carrete para que pase a la siguiente imagen
+                var nuevoMargen = marginLeft + 100;
+                if (nuevoMargen >= -200 && nuevoMargen <= 0) {
+                    carrete.style.marginLeft = nuevoMargen + "%";
+                }
+            });
+        }
+    
+        //Obtengo las flechas apuntando a la derecha de cada slider
+        var rightArrows = slider[i].getElementsByClassName("right");
+    
+        for (var j = 0; j < rightArrows.length; j++) {
+            rightArrows[i].addEventListener("click", function() {
+                var carrete = this.parentNode.children[1].children[0];
+                var marginLeft = parseInt(carrete.style.marginLeft);
+    
+                if (isNaN(marginLeft)) {
+                    marginLeft = 0;
+                }
+    
+                var nuevoMargen = marginLeft - 100;
+                if (nuevoMargen >= -200 && nuevoMargen <= 0) {
+                    carrete.style.marginLeft = nuevoMargen + "%";
+                }
+            });
+        }
+        
+    }
+    
 
-            const nuevoMargen = marginLeft - 100;
-            if (nuevoMargen >= -200 && nuevoMargen <= 0) {
-                carrete.style.marginLeft = nuevoMargen + "%";
-            }
-
-        });
-    });
 
 });
